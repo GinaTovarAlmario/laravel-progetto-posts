8dfb9aa22c3ea5d0cc9faf4baebf65c0285a8497
GinaTovarAlmario <ginatovaralmario@gmail.com>
2024-11-11 23:06:03 +0100

    added button edit show and delete

--- resources/views/admin/posts/index.blade.php
+++ resources/views/admin/posts/index.blade.php
@@ -24,12 +24,37 @@
                         </tr>
                     </thead>
                     <tbody>
-                        <tr>
-                            <th scope="row">1</th>
-                            <td>Mark</td>
-                            <td>Otto</td>
-                            <td>@mdo</td>
-                        </tr>
+                        @forelse ($posts as $post)
+                            <tr>
+                                <th>
+
+                                </th>
+                                <td>
+
+                                </td>
+                                <td>
+
+                                </td>
+                                <td>
+
+                                </td>
+                                <td>
+                                    <a href="{{route('admin.posts.show', $post)}}" class="btn btn-success">Show</a>
+                                    <a href="{{route('admin.posts.edit', $post->id)}}" class="btn btn-warning">Edit</a>
+                                    <form action="{{route('admin.posts.delete', $post->id)}}" method="POST">
+                                        @method('DELETE')
+                                        @csrf
+                                        <button type="submit">
+                                            DELETE
+                                        </button>
+                                    </form>
+                                </td>
+                            </tr>
+                        @empty
+                            <tr>
+                                No post available
+                            </tr>
+                        @endforelse
                     </tbody>
                 </table>
             </div>

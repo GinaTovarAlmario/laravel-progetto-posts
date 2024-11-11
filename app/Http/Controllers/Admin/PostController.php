50609681999f882cd7a352a122b2bb248b4f42e3
GinaTovarAlmario <ginatovaralmario@gmail.com>
2024-11-11 22:52:13 +0100

    added specifics to every crud

--- app/Http/Controllers/Admin/PostController.php
+++ app/Http/Controllers/Admin/PostController.php
@@ -67,7 +67,7 @@ class PostController extends Controller
     public function destroy(Post $post)
     {
         $post->delete();
-        return redirect()->route('admin.projects.index');
+        return redirect()->route('admin.posts.index');
 
     }
 }

5bc85fa4460a48472f8ea677abf9e99f32730669
GinaTovarAlmario <ginatovaralmario@gmail.com>
2024-11-11 22:52:25 +0100

    added basic structure index

--- resources/views/admin/posts/index.blade.php
+++ resources/views/admin/posts/index.blade.php
@@ -1,7 +1,38 @@
 @extends('layouts.app')
 
 @section('content')
-<h1>
-    ADMIN INDEX
-</h1>
+    <div class="container">
+        <div class="row">
+            <div class="col-12">
+                <h1 class="text-center fw-bold">
+                    Post list:
+                </h1>
+            </div>
+            <div class="col-12">
+                <div class="mb-3">
+                    <a href="{{ route('admin.posts.create') }}" class="btn btn-primary">
+                        Create Post
+                    </a>
+                </div>
+                <table class="table">
+                    <thead>
+                        <tr>
+                            <th scope="col">#</th>
+                            <th scope="col">First</th>
+                            <th scope="col">Last</th>
+                            <th scope="col">Handle</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <th scope="row">1</th>
+                            <td>Mark</td>
+                            <td>Otto</td>
+                            <td>@mdo</td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    </div>
 @endsection

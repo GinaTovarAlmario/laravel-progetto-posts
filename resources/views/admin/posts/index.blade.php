9a071c74538d2498794504223233d64607ab80ff
GinaTovarAlmario <ginatovaralmario@gmail.com>
2024-11-14 22:06:42 +0100

    added faker to make some data

--- resources/views/admin/posts/index.blade.php
+++ resources/views/admin/posts/index.blade.php
@@ -18,25 +18,23 @@
                     <thead>
                         <tr>
                             <th scope="col">#</th>
-                            <th scope="col">First</th>
-                            <th scope="col">Last</th>
-                            <th scope="col">Handle</th>
+                            <th scope="col">Title</th>
+                            <th scope="col">Author</th>
+                            <th scope="col">Content</th>
+                            <th scope="col">Actions</th>
                         </tr>
                     </thead>
                     <tbody>
                         @forelse ($posts as $post)
                             <tr>
                                 <th>
-
+                                    {{$post->id}}
                                 </th>
                                 <td>
-
+                                    {{$post->title}}
                                 </td>
                                 <td>
-
-                                </td>
-                                <td>
-
+                                    {{$post->author}}
                                 </td>
                                 <td>
                                     <a href="{{route('admin.posts.show', $post)}}" class="btn btn-success">Show</a>

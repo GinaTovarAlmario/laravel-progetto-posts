563f0d62cd69746d2fa7842affbf5748bf2a9ff7
GinaTovarAlmario <ginatovaralmario@gmail.com>
2024-11-14 22:06:27 +0100

    added properties to table

--- database/migrations/2024_11_08_231450_create_posts_table.php
+++ database/migrations/2024_11_08_231450_create_posts_table.php
@@ -13,6 +13,9 @@ return new class extends Migration
     {
         Schema::create('posts', function (Blueprint $table) {
             $table->id();
+            $table->string('title',255);
+            $table->string('author',50);
+            $table->text('content');
             $table->timestamps();
         });
     }

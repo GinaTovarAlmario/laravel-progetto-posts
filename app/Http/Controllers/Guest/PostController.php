25738586e5b06d07309d82c2464a9dc408567d0d
GinaTovarAlmario <ginatovaralmario@gmail.com>
2024-11-17 00:50:43 +0100

    added route show index guest

--- app/Http/Controllers/Guest/PostController.php
+++ app/Http/Controllers/Guest/PostController.php
@@ -3,9 +3,16 @@
 namespace App\Http\Controllers\Guest;
 
 use App\Http\Controllers\Controller;
+use App\Models\Post;
 use Illuminate\Http\Request;
 
 class PostController extends Controller
 {
-    //
+    public function index(){
+        $posts = Post::paginate(6);
+        return view('guest.posts.index',compact('posts'));
+    }
+    public function show(Post $post){
+        return view('guest.posts.show', compact('post'));
+    }
 }

e89ee388b02ceb6456c4bceb564a53a4b44ef016
GinaTovarAlmario <ginatovaralmario@gmail.com>
2024-11-17 00:48:22 +0100

    added basic structure for guest

---
+++ resources/views/guest/posts/index.blade.php
@@ -0,0 +1,46 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="container">
+        <div class="row">
+            <div class="col-12">
+                <h1 class="text-center fw-bold">
+                    Post list:
+                </h1>
+            </div>
+            <div class="col-12">
+                <table class="table">
+                    <thead>
+                        <tr>
+                            <th scope="col">#</th>
+                            <th scope="col">Title</th>
+                            <th scope="col">Author</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        @forelse ($posts as $post)
+                            <tr>
+                                <th>
+                                    {{$post->id}}
+                                </th>
+                                <td>
+                                    {{$post->title}}
+                                </td>
+                                <td>
+                                    {{$post->author}}
+                                </td>
+                            </tr>
+                        @empty
+                            <tr>
+                                No post available
+                            </tr>
+                        @endforelse
+                    </tbody>
+                </table>
+                <div>
+                    {{$posts->links()}}
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection

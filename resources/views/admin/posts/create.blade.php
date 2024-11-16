cbc80fac495a45055e798bf1ed5fc39ca863b5a2
GinaTovarAlmario <ginatovaralmario@gmail.com>
2024-11-16 23:46:11 +0100

    added position

--- resources/views/admin/posts/create.blade.php
+++ resources/views/admin/posts/create.blade.php
@@ -2,25 +2,25 @@
 @section('content')
     <div class="container">
         <div class="row">
-            <div class="col-12">
+            <div class="col-6 my-0 mx-auto">
                 <form action="{{route('admin.posts.store')}}" method="POST">
                     @method("POST")
                     @csrf
                     <div class="mb-3">
                         <label for="title">Title</label>
-                        <input type="text" name="title" id="title">
+                        <input type="text" name="title" id="title" class="form-control">
                     </div>
                     <div class="mb-3">
                         <label for="author">Author</label>
-                        <input type="text" name="author" id="author">
+                        <input type="text" name="author" id="author" class="form-control">
                     </div>  <div class="mb-3">
                         <label for="content">Content</label>
-                        <textarea name="content" id="content" cols="30" rows="10">
+                        <textarea name="content" id="content" cols="25" rows="8" class="form-control">
 
                         </textarea>
                     </div>
-                    <button type="submit">Create</button>
-                    <button type="reset">Reset</button>
+                    <button type="submit" class="btn btn-primary">Create</button>
+                    <button type="reset" class="btn btn-success">Reset</button>
                 </form>
             </div>
         </div>

b84c47a6f33aa0cdea8cd4eb0d1233a16275ce47
GinaTovarAlmario <ginatovaralmario@gmail.com>
2024-11-16 23:57:39 +0100

    implemented create

--- resources/views/admin/posts/create.blade.php
+++ resources/views/admin/posts/create.blade.php
@@ -3,21 +3,37 @@
     <div class="container">
         <div class="row">
             <div class="col-6 my-0 mx-auto">
-                <form action="{{route('admin.posts.store')}}" method="POST">
-                    @method("POST")
+                <form action="{{ route('admin.posts.store') }}" method="POST">
+                    @method('POST')
                     @csrf
                     <div class="mb-3">
                         <label for="title">Title</label>
-                        <input type="text" name="title" id="title" class="form-control">
+                        <input type="text" name="title" id="title" class="form-control" value="{{old('title')}}">
+                        @error('title')
+                            <div class="alert alert-warning">
+                                {{ $mesage }}
+                            </div>
+                        @enderror
                     </div>
                     <div class="mb-3">
                         <label for="author">Author</label>
-                        <input type="text" name="author" id="author" class="form-control">
-                    </div>  <div class="mb-3">
+                        <input type="text" name="author" id="author" class="form-control" value="{{old('author')}}">
+                        @error('author')
+                            <div class="alert alert-warning">
+                                {{ $mesage }}
+                            </div>
+                        @enderror
+                    </div>
+                    <div class="mb-3">
                         <label for="content">Content</label>
                         <textarea name="content" id="content" cols="25" rows="8" class="form-control">
-
+                            {{old('content')}}
                         </textarea>
+                        @error('content')
+                            <div class="alert alert-warning">
+                                {{ $mesage }}
+                            </div>
+                        @enderror
                     </div>
                     <button type="submit" class="btn btn-primary">Create</button>
                     <button type="reset" class="btn btn-success">Reset</button>

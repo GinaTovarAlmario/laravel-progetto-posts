d79830436dd9c4163414b0beebf249ebd1b04078
GinaTovarAlmario <ginatovaralmario@gmail.com>
2024-11-17 01:04:16 +0100

    added show guest

---
+++ resources/views/guest/posts/show.blade.php
@@ -0,0 +1,23 @@
+@extends('layouts.app')
+@section('content')
+    <div class="container">
+        <div class="row">
+            <div class="col-12">
+                <div class="card">
+                    <div class="card-body text-black">
+                        <h5 class="card-title">
+                            TITLE: {{$post->title}}
+                        </h5>
+                        <h6 class="card-subtitle mb-2 text-body-secondary">
+                            AUTHOR: {{$post->author}}
+                        </h6>
+                        <p class="card-text">
+                            DESCRIPTION: {{ $post->content }}
+                        </p>
+                        <a href="{{route('guest.posts.index')}}" class="btn btn-primary">Go Back</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection

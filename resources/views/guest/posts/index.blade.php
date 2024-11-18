9a3b7228433807899b17138a9fa9edef1073fd2f
GinaTovarAlmario <ginatovaralmario@gmail.com>
2024-11-18 18:17:22 +0100

    added style and pagination

--- resources/views/guest/posts/index.blade.php
+++ resources/views/guest/posts/index.blade.php
@@ -9,17 +9,23 @@
                 </h1>
             </div>
             <div class="col-12">
-                <div class="row">
+                <div class="row justify-content-center">
                     @forelse ($posts as $index => $post)
-                        <div class="col-3 me-2 mb-3">
-                            {{$post->id}}
-                            {{$post->title}}
-                            {{$post->author}}
-                            {{$post->content}}
+                        <div class="col-3 mt-3 mb-4 me-2 card">
+                            <h2>
+                                {{$post->id}} - {{$post->title}}
+                            </h2>
+                            <h3>
+                                {{$post->author}}
+                            </h3>
+                            <p class="fs-4">
+                                {{substr($post->content,0,40) }}..
+                            </p>
+                            <a href="{ route('guest.posts.show',$post)}" class="btn btn-outline-primary mb-3">Read More</a>
                         </div>
                     @empty
-                        <div>
-                            No posts available
+                        <div class="col-12">
+                            <h1>No posts available</h1>
                         </div>
                     @endforelse
                 <div>
